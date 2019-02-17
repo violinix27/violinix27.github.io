a497c33c4c117b657733b6f667236d6e3a811bd8
violinix27 <46071418+violinix27@users.noreply.github.com>
2019-02-17 20:10:13 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
-
+<DOCTYPE html>
 <?php
 $url = "https://api.openweathermap.org/data/2.5/weather?id=2886241&type=accurate&units=metric&mode=xml&APPID=baf12c4cd63ad729832998400da8d602";
 $getweather = simplexml_load_file($url);
-//$gethumidity = $getweather->city->humidity['value'];
-//$gettemp = $getweather->city id="2886241" name="Regierungsbezirk Köln"->temperature['value'];
+$gethumidity = $getweather->city->humidity['value'];
+$gettemp = $getweather->city id="2886241" name="Regierungsbezirk Köln"->temperature['value'];
 ?>
 <html>
 <head>
@@ -15,7 +15,7 @@ $getweather = simplexml_load_file($url);
     <p>Das testet die Openweather API:</p>
 	<ul>
 		<li>Feuchtigkeit: <?php echo($gethumidity)?>%</li>
-		<li>Temperatur: <?php echo('TEST')?> &deg;C<li/>
+		<li>Temperatur: <?php echo($gettemp)?> &deg;C<li/>
 	<ul/>
 </body>
 </html>
